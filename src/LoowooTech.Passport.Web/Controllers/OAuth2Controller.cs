676b6042cb5e2c258fee45d402740b163bddb03f
Mad <zhengliangjun@gmail.com>
2013-10-15 11:57:10 +0800

    add oauth2controller

---
+++ src/LoowooTech.Passport.Web/Controllers/OAuth2Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoowooTech.Passport.Web.Controllers
+{
+    [UserAuthorize]
+    public class OAuth2Controller : Controller
+    {
+        public ActionResult Authroize(string client_id, string redirect_url)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public ActionResult AccessToken(string client_id, string client_secret, string code)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
